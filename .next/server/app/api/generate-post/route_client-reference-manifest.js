a8adbbe110b8161da61c664b501d003134f03e90
Hardik Mehta <Hmhardik5021@gmail.com>
2025-12-25 00:44:24 +0530

    Delete .next directory

--- .next/server/app/api/generate-post/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/api/generate-post/route"] = {"moduleLoading":{"prefix":"/_next/","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"slarmpP6Lmk77UwTnANhwxCTVZ1/xFJecpTVs1celmo=\"\n}"
